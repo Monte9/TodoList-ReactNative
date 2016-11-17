46106f6c17b012af67f209d1925d253a7e5ff1d5
Monte9 <Monte9@users.noreply.github.com>
2016-11-17 13:52:07 -0800

    images added

--- app/components/Checkbox.js
+++ app/components/Checkbox.js
@@ -1,11 +1,29 @@
 import React, { Component, PropTypes } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 
 const styles = StyleSheet.create({
+  checkbox: {
+    flex: 1,
+    padding: 10,
+  },
+  checkboxImage: {
+    height: 30,
+    width: 30,
+  }
 })
 
 export default class Checkbox extends Component {
+  checkItem() {
+    console.log("checked")
+  }
+
   render() {
-    return null
+    return (
+      <TouchableOpacity style={styles.checkbox} onPress={this.checkItem.bind(this)}>
+        <Image
+          style={styles.checkboxImage}
+          source={require('../img/checked.png')}/>
+      </TouchableOpacity>
+    )
   }
 }
--- app/components/Item.js
+++ app/components/Item.js
@@ -1,7 +1,8 @@
 import React, { Component, PropTypes } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native'
 
 import Checkbox from './Checkbox'
+
 
 const styles = StyleSheet.create({
   itemView: {
@@ -15,20 +16,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   item: {
-    flex: 5,
+    flex: 10,
     padding: 10,
   },
   checkbox: {
     flex: 1,
+  },
+  delete: {
+    flex: 1,
+  },
+  deleteImage: {
+    height: 18,
+    width: 18,
   }
 })
 
 export default class Item extends Component {
+  deleteItem() {
+    console.log("Delete item")
+  }
+
   render() {
     return(
       <View style={styles.itemView}>
         <Text style={styles.item}>{this.props.item}</Text>
         <Checkbox style={styles.checkbox}/>
+        <TouchableOpacity style={styles.delete} onPress={this.deleteItem.bind(this)}>
+          <Image
+            style={styles.deleteImage}
+            source={require('../img/delete.png')}/>
+        </TouchableOpacity>
       </View>
     )
   }
